fd0496c7f8134ec0e4d73c73c2970c01b1fa05a4
gukodev <gugu.r.azevedo@gmail.com>
2024-01-18 14:06:47 -0300

    chagnet the name

--- src/components/EmojiGenerator/Tooltip.tsx
+++ src/components/EmojiGenerator/Tooltip.tsx
@@ -9,8 +9,8 @@ interface TooltipProps {
 }
 
 export default function Tooltip({ text, show, x, y }: TooltipProps) {
-    const tcfg = {
-        transition: 'transition-[opacity,transform] duration-100',
+    const t = {
+        transition: 'transition-[opacity,transform] duration-100 ease-out',
         from: 'opacity-0 scale-75',
         to: 'opacity-100 scale-100',
     }
@@ -18,12 +18,12 @@ export default function Tooltip({ text, show, x, y }: TooltipProps) {
     return (
         <Transition
             show={show}
-            enter={tcfg.transition}
-            leave={tcfg.transition}
-            enterFrom={tcfg.from}
-            leaveTo={tcfg.from}
-            enterTo={tcfg.to}
-            leaveFrom={tcfg.to}
+            enter={t.transition}
+            leave={t.transition}
+            enterFrom={t.from}
+            leaveTo={t.from}
+            enterTo={t.to}
+            leaveFrom={t.to}
             className={twMerge(
                 'fixed z-10 rounded-lg p-1 leading-none pointer-events-none select-none',
                 'text-sm text-slate-200 border-[1px] border-slate-500 bg-slate-700'
